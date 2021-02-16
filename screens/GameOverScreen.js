cd2c8079df896cbfef173f3ad3206f9b05c72011
Kaan Er <erkaan1@outlook.com>
2021-02-16 22:10:07 +0300

    Edit game-over-screen style

--- screens/GameOverScreen.js
+++ screens/GameOverScreen.js
@@ -1,5 +1,13 @@
 import React from "react";
-import { View, StyleSheet, Button, Image, Text } from "react-native";
+import {
+  View,
+  StyleSheet,
+  Button,
+  Image,
+  Text,
+  Dimensions,
+  ScrollView,
+} from "react-native";
 import BodyText from "../components/BodyText";
 import MainButton from "../components/MainButton";
 import TitleText from "../components/TitleText";
@@ -7,32 +15,34 @@ import colors from "../constants/colors";
 
 const GameOverScreen = (props) => {
   return (
-    <View style={styles.screen}>
-      <TitleText>The Game is Over!</TitleText>
-      <View style={styles.imageContainer}>
-        <Image
-          // fadeDuration={1000} web'ten resim yüklenirken verilen effect default olarak 300ms.
-          // image source local
-          source={require("../assets/success.png")}
-          // image source web
-          // source={{
-          //   uri:
-          //     "https://www.google.com.tr/logos/doodles/2021/valentines-day-2021-6753651837108860.3-law.gif",
-          // }}
-          style={styles.image}
-          resizeMode="cover"
-        />
+    <ScrollView>
+      <View style={styles.screen}>
+        <TitleText>The Game is Over!</TitleText>
+        <View style={styles.imageContainer}>
+          <Image
+            // fadeDuration={1000} web'ten resim yüklenirken verilen effect default olarak 300ms.
+            // image source local
+            source={require("../assets/success.png")}
+            // image source web
+            // source={{
+            //   uri:
+            //     "https://www.google.com.tr/logos/doodles/2021/valentines-day-2021-6753651837108860.3-law.gif",
+            // }}
+            style={styles.image}
+            resizeMode="cover"
+          />
+        </View>
+        <View style={styles.resultContainer}>
+          <BodyText style={styles.resultText}>
+            Your phone needed
+            <Text style={styles.highlight}> {props.roundsNumber}</Text> rounds
+            to guess the number
+            <Text style={styles.highlight}> {props.userNumber}</Text>.
+          </BodyText>
+        </View>
+        <MainButton onPress={props.onRestart}>NEW GAME</MainButton>
       </View>
-      <View style={styles.resultContainer}>
-        <BodyText style={styles.resultText}>
-          Your phone needed
-          <Text style={styles.highlight}> {props.roundsNumber}</Text> rounds to
-          guess the number
-          <Text style={styles.highlight}> {props.userNumber}</Text>.
-        </BodyText>
-      </View>
-      <MainButton onPress={props.onRestart}>NEW GAME</MainButton>
-    </View>
+    </ScrollView>
   );
 };
 
@@ -43,13 +53,13 @@ const styles = StyleSheet.create({
     alignItems: "center",
   },
   imageContainer: {
-    width: "80%",
-    height: 300,
-    borderRadius: 200,
+    width: Dimensions.get("window").width * 0.7,
+    height: Dimensions.get("window").width * 0.7,
+    borderRadius: (Dimensions.get("window").width * 0.7) / 2,
     borderWidth: 3,
     borderColor: "black",
     overflow: "hidden",
-    marginVertical: 30,
+    marginVertical: Dimensions.get("window").height / 30,
   },
   image: {
     width: "100%",
@@ -57,11 +67,11 @@ const styles = StyleSheet.create({
   },
   resultContainer: {
     marginHorizontal: 30,
-    marginVertical: 15,
+    marginVertical: Dimensions.get("window").height / 60,
   },
   resultText: {
     textAlign: "center",
-    fontSize: 20,
+    fontSize: Dimensions.get("window").height < 400 ? 16 : 20,
   },
   highlight: {
     color: colors.primary,
--- screens/GameScreen.js
+++ screens/GameScreen.js
@@ -6,6 +6,7 @@ import {
   Alert,
   ScrollView,
   FlatList,
+  Dimensions,
 } from "react-native";
 import Card from "../components/Card";
 import { AntDesign } from "@expo/vector-icons";
@@ -115,13 +116,13 @@ const styles = StyleSheet.create({
   buttonContainer: {
     flexDirection: "row",
     justifyContent: "space-around",
-    marginTop: 20,
+    marginTop: Dimensions.get("window").height > 600 ? 20 : 5,
     width: 400,
     maxWidth: "90%",
   },
   listContainer: {
     flex: 1, // android de kaydırma yapabilmek için ekledik.
-    width: "60%",
+    width: Dimensions.get("window").width > 350 ? "60%" : "80%",
   },
   list: {
     flexGrow: 1, // android de kaydırma yapabilmek için ekledik.
